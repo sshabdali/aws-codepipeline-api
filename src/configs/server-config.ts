1aae95a271c19fb5500a2e9e2437885b338a3f60
Shafaat Hussain <sshabdali@hotmail.com>
2020-07-15 14:40:44 +0100

    fixed Mongodb connection

--- src/configs/server-config.ts
+++ src/configs/server-config.ts
@@ -3,7 +3,7 @@ import { Config } from '../models/config';
 export const serverConfig = {
     getConfigs: (): Config => {
         return {
-            MongodbConnection: process.env.MongodbConnection || `mongodb://${process.env.MongoHost || 'localhost'}:27017/PracticeDB`,
+            MongodbConnection: `mongodb+srv://eatniaAdmin:wCxQf5X9qP89BmXJ@clusteralpha-f7lwc.mongodb.net/PracticeDB?retryWrites=true&w=majority`,
             ServiceHost: 'localhost',
             ServicePort: process.env.PORT || '3000'
         };
@@ -11,5 +11,6 @@ export const serverConfig = {
 };
 
 
+// process.env.MongodbConnection || `mongodb://${process.env.MongoHost || 'localhost'}:27017/PracticeDB`,
 // `mongodb+srv://eatniaAdmin:wCxQf5X9qP89BmXJ@clusteralpha-f7lwc.mongodb.net/PracticeDB?retryWrites=true&w=majority`
 // `mongodb://${process.env.MongoHost || 'localhost'}:27017/PracticeDB`
